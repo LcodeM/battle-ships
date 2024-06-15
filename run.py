d215c49a184212c0df87b078f267d20bb03a2813
Lewis Mayhew <lewis_mayhew@hotmail.co.uk>
2024-06-15 09:29:54 +0000

    Define dictionary letter to numbers for y-axis inputs

--- run.py
+++ run.py
@@ -2,3 +2,9 @@
 CPU_BOARD = [[' '] * 8 for x in range(8)]
 # Board for user guesses to be placed, using list of empty spaces
 PLAYER_BOARD = [[' '] * 8 for x in range(8)]
+
+# Convert letters to numbers for user input using dictionary:
+# (0, 0) input would be A1 on board axis
+# Ref CI LMS dictionary comprehensions
+y_axis_converter = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}
+
