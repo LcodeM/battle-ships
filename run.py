582ccf5802dc8078104ad3322fc28775d9c97fa4
Lewis Mayhew <lewis_mayhew@hotmail.co.uk>
2024-06-15 11:18:29 +0000

    Fix. y axis converter not using 0 indexing. Hits registering 1 place to right

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ PLAYER_BOARD = [[' '] * 8 for x in range(8)]
 # Convert letters to numbers for user input using dictionary:
 # (0, 0) input would be A1 on board axis
 # Ref CI LMS dictionary comprehensions
-y_axis_converter = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}
+y_axis_converter = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
 def generate_board(board):
@@ -56,7 +56,7 @@ def choose_row():
         # Look for integer input between 1 and 8
         try:
             row = input('Please enter a row 1-8\n')
-            if row not in '1234567':
+            if row not in '12345678':
                 # If input not valid, return error and run again
                 raise ValueError("Invalid row")
             # Re-run until valid
@@ -65,7 +65,7 @@ def choose_row():
         except ValueError as e:
             print(f"Error: {e}\n Please enter a valid row")
     # Return our number and adjust for 0 indexing
-    return int(row) -1
+    return int(row) - 1
 
 
 def choose_column():
